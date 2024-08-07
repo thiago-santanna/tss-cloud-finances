daaa18fab7e7210302aa80067e62242a11668640
thiago-santanna <uzmaki_thiago@outlook.com>
2024-08-07 14:17:03 -0300

    logo marca e menu

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/header";
+
 export default function Dashboard() {
   return (
     <>
-      <p>Dashboard</p>
+      <Header />
     </>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <>
+      <header className="bg-green-900 h-40 container mx-auto px-4 flex justify-between items-start pt-4">
+        <Image
+          src="/logo.svg"
+          alt="Logo Finances"
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+        <div className="flex gap-4 text-slate-100">
+          <div className="cursor-pointer border-b-0 hover:text-slate-400 hover:border-b-green-400 hover:border-b-2">
+            Listagem
+          </div>
+          <div className="cursor-pointer border-b-0 hover:text-slate-400 hover:border-b-green-400 hover:border-b-2">
+            Importar
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
